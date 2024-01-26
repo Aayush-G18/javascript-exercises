a4b2746d4e0dc2f25f9af18292fa936bad9716d1
Aayush-G18 <goyalaayush292@gmail.com>
2024-01-27 01:23:46 +0530

    Basic method

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a1,a2) {
+    let sum=0;
+    for (let i=a1;i=a2;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
