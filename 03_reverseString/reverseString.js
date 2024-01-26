9022dda3783b14f0b3ddadc0301a8b0ee86d1817
Aayush-G18 <goyalaayush292@gmail.com>
2024-01-27 00:56:24 +0530

    First 3 solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str.length<2){
+        return str;
+    }else{
+        let final='';
+        for (let i=str.length-1;i>=0;i--){
+            final=final.concat(str[i]);
+        }
+        return final;
+    }
 };
 
 // Do not edit below this line
