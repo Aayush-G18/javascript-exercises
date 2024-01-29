551ec5437d7a22159035a7d03cc1e79e769aff81
Aayush-G18 <goyalaayush292@gmail.com>
2024-01-29 18:55:44 +0530

    Palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (original) {
+    original=original.toLowerCase().replace(/[^a-z0-9]/g,"");
+    return (original===original.split("").reverse().join(""));
 };
 
 // Do not edit below this line
